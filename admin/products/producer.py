c18dfdb3916cd4e737dbcdda2c8f59616495d6a9
Herbert Lin <herbert.lin.7@gmail.com>
2022-01-09 22:41:59 -0600

    Update flask database when any change is made in django products

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://fpirdcnu:f7tQVuLEYK7MlJNkYJJn_cDoegId2e_q@shrimp.rmq.cloudamqp.com/fpirdcnu')
 
@@ -6,5 +6,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
--- main/main.py
+++ main/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, jsonify, abort
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
+
+from producer import publish
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
@@ -9,11 +14,17 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
+@dataclass
 class ProductUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
@@ -21,9 +32,10 @@ class ProductUser(db.Model):
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'Hello'
+    return jsonify(Product.query.all())
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
